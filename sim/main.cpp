6b167f426b015f6415bc84587e5fac516906e73f
Leonardo Celente <leocelente@gmail.com>
2022-03-10 13:32:40 -0300

    Added wave file generation. Also moved my classes to seperate file

--- sim/main.cpp
+++ sim/main.cpp
@@ -1,30 +1,28 @@
 
-// Include common routines
-#include "verilated.h"
-#include <memory>
 // Include model header, generated from Verilating "top.v"
+#include "DUT.hpp"
 #include "Vtop.h"
 
-struct DUT {
-  std::unique_ptr<Vtop> top;
+template <> void DUT<Vtop>::run(vluint64_t max_cycles) {
+  // initialize input/output signals
+  top->clock_in = 0;
+  top->reset_in = 0;
+  top->direction_in = 1;
 
-  [[nodiscard]] DUT() { top = std::make_unique<Vtop>(); }
-
-  void run() {
-    while (!Verilated::gotFinish()) {
-      // Evaluate model
-      top->eval();
-    }
+  // Simulation Loop
+  for (cycles = 0; !Verilated::gotFinish() && cycles < max_cycles; ++cycles) {
+    top->clock_in ^= 1; // toggle clock
+    top->eval();
+    tracer.m_trace->dump(cycles);
   }
+}
 
-  ~DUT() { top->final(); }
-};
+int main(int argc, char **argv, [[maybe_unused]] char **env) {
 
-int main([[maybe_unused]] int argc, [[maybe_unused]] char **argv,
-         [[maybe_unused]] char **env) {
+  Verilated::commandArgs(argc, argv);
 
-  DUT dut{};
-  dut.run();
+  DUT<Vtop> dut{};
+  dut.run(64);
 
   return 0;
 }
